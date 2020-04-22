98a83e1f05fc777e55e75e801887cd7c8e96c851
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-22 12:57:14 +0530

    first commit

---
+++ src/javapro/sum.java
@@ -0,0 +1,11 @@
+package javapro;
+
+public class sum 
+{
+
+	public static void main(String[] args) 
+	{
+      System.out.println("Hello javaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	}
+
+}
